496023a6ee074634674ea48449ac3dace7bfa76c
Rodrigo Ernesto Mendoza Alfaro <MA24005>
2026-09-23 00:16:22 -0600

    Resolviendo conflicto de las clases Test

--- src/test/java/sv/edu/ues/occ/ingenieria/ppi115_2026/salud/galenosv/control/ExamenTipoExamenDAOTest.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/ppi115_2026/salud/galenosv/control/ExamenTipoExamenDAOTest.java
@@ -1,5 +1,7 @@
 package sv.edu.ues.occ.ingenieria.ppi115_2026.salud.galenosv.control;
 
+package sv.edu.ues.occ.ingenieria.ppi115_2026.salud.galenosv.control;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Test;
